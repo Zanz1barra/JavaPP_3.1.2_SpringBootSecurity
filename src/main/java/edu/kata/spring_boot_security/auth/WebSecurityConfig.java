ec5dfea5aca78a1a800412d126ea286951e46cd4
Pavel Naboka <Pavmn96@yandex.ru>
2025-03-29 19:01:04 +0300

    Created the `WebSecurityConfig` configuration class, responsible for configuring authentication and authorization. Created the `AuthSuccessHandler` handler class, responsible for redirecting the user after authentication to a page that matches their authorization level. Created the `AuthUserService` service class and the `UserRepository` repository interface, responsible for interacting with the user authentication data table.

---
+++ src/main/java/edu/kata/spring_boot_security/auth/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package edu.kata.spring_boot_security.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/home", "/public/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+//                        .defaultSuccessUrl("/dashboard")
+                        .successHandler(new AuthSuccessHandler())
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                )
+                .userDetailsService(userDetailsService) // Указываем наш сервис загрузки пользователей
+                .csrf(AbstractHttpConfigurer::disable); // В продакшене лучше включить CSRF
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
